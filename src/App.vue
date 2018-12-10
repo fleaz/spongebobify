3a3ccfb53e07352c9cc1c5fd51fa6a5cdee30066
Felix Breidenstein <mail@felixbreidenstein.de>
2018-12-10 23:55:33 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Spongebob />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Spongebob from './components/Spongebob.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Spongebob
   }
 }
 </script>
@@ -23,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Spongebob.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="content">
+    <input id="input" size=80 v-on:keyup="update" v-model="text">
+    <span id="output">{{ output }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spongebob",
+  data: function() {
+    return {
+      text: "",
+      output: ""
+    };
+  },
+  methods: {
+    update: function() {
+      this.output = this.alternatingCase(this.text);
+    },
+    alternatingCase: function(input) {
+      let output = ""
+      let caps = true 
+      input.split('').forEach(element => {
+        if (caps) {
+          output +=  element.toUpperCase();
+        } else {
+          output +=  element.toLowerCase();
+        }
+        caps = !caps;
+      });
+
+      return output
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+#input {
+  margin: 50px;
+  font-size: 1em;
+}
+
+#output {
+  margin: 50px;
+  width: 100%;
+  float: left;
+  font-family: Impact;
+  font-size: 2.5em;
+  color: white;
+  text-shadow:2px 2px 0 #000,
+  -2px -2px 0 #000,
+  2px -2px 0 #000,
+  -2px 2px 0 #000,
+  0px 2px 0 #000,
+  2px 0px 0 #000,
+  0px -2px 0 #000,
+  -2px 0px 0 #000,
+  2px 2px 5px #000;
+
+}
+.content:before {
+  content: "";
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: -1;
+
+  display: block;
+  background-image: url("/mocking-spongebob.jpg");
+  width: 100%;
+  height: 100%;
+
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  -webkit-filter: blur(5px);
+  -moz-filter: blur(5px);
+  -o-filter: blur(5px);
+  -ms-filter: blur(5px);
+  filter: blur(5px);
+}
+</style>
